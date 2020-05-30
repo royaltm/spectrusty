d759622ae558609f23f447394f0af7debb594b0c
Rafal Michalski <royaltm75@gmail.com>
2020-05-30 21:54:16 +0200

    core: memory snapshot compression feature

--- spectrusty_core/src/memory/serde.rs
+++ spectrusty_core/src/memory/serde.rs
@@ -1,8 +1,15 @@
 //! Utilities for serializing memory as base64 strings or just bytes in binary serializers.
+#[cfg(feature = "compression")] use core::iter::FromIterator;
 use core::fmt;
 use std::rc::Rc;
 
-use serde::{Serializer, Deserialize, Deserializer, de::{self, Visitor}};
+#[cfg(feature = "compression")] use compression::prelude::*;
+#[cfg(feature = "compression")] use serde::ser;
+
+use serde::{
+    Serializer, Deserialize, Deserializer,
+    de::{self, Visitor}
+};
 
 use super::{MEM32K_SIZE, MEM48K_SIZE, MEM64K_SIZE, MEM128K_SIZE};
 
@@ -10,11 +17,28 @@ pub fn serialize_mem<T, S>(mem: &T, serializer: S) -> Result<S::Ok, S::Error>
     where T: MemSerExt,
           S: Serializer
 {
+    #[cfg(not(feature = "compression"))]
+    {
+        serialize_mem_slice(mem.as_slice(), serializer)
+    }
+    #[cfg(feature = "compression")]
+    {
+        let compr = mem.as_slice().into_iter().copied()
+            .encode(&mut GZipEncoder::new(), Action::Finish)
+            .collect::<Result<Vec<_>, _>>()
+            .map_err(ser::Error::custom)?;
+        serialize_mem_slice(&compr, serializer)
+    }
+}
+
+pub fn serialize_mem_slice<S>(slice: &[u8], serializer: S) -> Result<S::Ok, S::Error>
+    where S: Serializer
+{
     if serializer.is_human_readable() {
-        serializer.serialize_str(&base64::encode(mem.as_slice()))
+        serializer.serialize_str(&base64::encode(slice))
     }
     else {
-        serializer.serialize_bytes(mem.as_slice())
+        serializer.serialize_bytes(slice)
     }
 }
 
@@ -24,7 +48,7 @@ pub fn deserialize_mem<'de, T, D>(deserializer: D) -> Result<T, D::Error>
 {
     if deserializer.is_human_readable() {
         Deserialize::deserialize(deserializer).and_then(|string: &str|
-            base64::decode(string).map_err(|err| de::Error::custom(err.to_string()) )
+            base64::decode(string).map_err(de::Error::custom)
         )
         .and_then(T::try_from_byte_buf)
     }
@@ -59,7 +83,14 @@ macro_rules! impl_mem_ser_de_ext {
         impl MemDeExt for Box<[u8;$len]> {
             const PREFER_FROM_BYTE_BUF: bool = true;
 
-            fn try_from_byte_buf<E: de::Error>(buf: Vec<u8>) -> Result<Self, E> {
+            #[allow(unused_mut)]
+            fn try_from_byte_buf<E: de::Error>(mut buf: Vec<u8>) -> Result<Self, E> {
+                #[cfg(feature = "compression")]
+                {
+                    if is_compressed(&buf) {
+                        buf = decompress(&buf)?;
+                    }
+                }
                 if buf.len() == $len {
                     let buf = buf.into_boxed_slice();
                     Ok(unsafe { Box::from_raw(Box::into_raw(buf) as *mut [u8; $len]) })
@@ -73,6 +104,12 @@ macro_rules! impl_mem_ser_de_ext {
             }
 
             fn try_from_bytes<E: de::Error>(slice: &[u8]) -> Result<Self, E> {
+                #[cfg(feature = "compression")]
+                {
+                    if is_compressed(slice) {
+                        return Self::try_from_byte_buf(decompress(slice)?)
+                    }
+                }
                 Self::try_from_byte_buf(Vec::from(slice))
             }
         }
@@ -102,10 +139,22 @@ impl MemDeExt for Rc<[u8]> {
     const PREFER_FROM_BYTE_BUF: bool = false;
 
     fn try_from_byte_buf<E: de::Error>(buf: Vec<u8>) -> Result<Self, E> {
+        #[cfg(feature = "compression")]
+        {
+            if is_compressed(&buf) {
+                return decompress(&buf)
+            }
+        }
         Ok(Rc::from(buf))
     }
 
     fn try_from_bytes<E: de::Error>(slice: &[u8]) -> Result<Self, E> {
+        #[cfg(feature = "compression")]
+        {
+            if is_compressed(slice) {
+                return decompress(slice)
+            }
+        }
         Ok(Rc::from(slice))
     }
 }
@@ -135,4 +184,21 @@ impl Visitor<'_> for ByteBufVisitor {
         Ok(Vec::from(v))
     }
 
+}
+
+#[cfg(feature = "compression")]
+fn is_compressed(data: &[u8]) -> bool {
+    match data.get(0..3) {
+        Some(&[0x1f, 0x8b, 0x08]) => true,
+        // Some(&[b'B', b'Z', b'h', b'1'..=b'9', 0x31, 0x41, 0x59, 0x26, 0x53, 0x59]) => true
+        _ => false
+    }
+}
+
+#[cfg(feature = "compression")]
+fn decompress<T: FromIterator<u8>, E: de::Error>(data: &[u8]) -> Result<T, E> {
+    data.into_iter().copied()
+        .decode(&mut GZipDecoder::new())
+        .collect::<Result<T, _>>()
+        .map_err(de::Error::custom)
 }
